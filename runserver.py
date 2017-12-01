606cf76b6cfa84475828974cf3c95562b909b2d2
Louis-Philippe Brochu <lpbrochu@gmail.com>
2017-12-01 05:54:22 -0500

    Added possible Google Engine config

--- runserver.py
+++ runserver.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from controller.notificationcontroller import NotificationController
+from notificationcontroller import NotificationController
 
 app = Flask(__name__)
 
@@ -8,8 +8,9 @@ controller = NotificationController
 
 @app.route("/users/<user_id>/notifications", methods=['GET'])
 def get_all_notifications(user_id):
-    return controller.retrieve_notifications(user_id=user_id)
+    return "hello world"
+    #return controller.retrieve_notifications(user_id=user_id)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
